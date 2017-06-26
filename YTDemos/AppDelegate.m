97b831c960af4efd5d8b9a8d8d40c840ed1d8bac
caixia-sun <caixia.sun@msparis.com>
2017-06-26 18:19:40 +0800

    test branch

--- YTDemos/AppDelegate.m
+++ YTDemos/AppDelegate.m
@@ -22,7 +22,7 @@
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
     
-    
+    //这是master分支
     
     self.window = [[UIWindow alloc] initWithFrame:[UIScreen mainScreen].bounds];
     self.window.backgroundColor = [UIColor whiteColor];
